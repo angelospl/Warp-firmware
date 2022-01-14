7e0caf17cea8785a364818c9d96b25a4cb33f604
Angelos Plevris <angelospl48@gmail.com>
2022-01-14 16:46:02 +0200

    Fix lowPassFilter61.c

--- src/boot/ksdk1.1.0/lowPassFilter61.c
+++ src/boot/ksdk1.1.0/lowPassFilter61.c
@@ -104,6 +104,8 @@ lowPassFilter()
 		sig[i] = (float) getXAccelerationMeasurement();
 	}
 
+	lowPassFilterOrder61(sig,filtered,SIZE);
+
 	for (int i = 0; i < SIZE; i++)
 	{
 		// printf("%lf\n",filtered[i]);
